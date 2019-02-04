75ea72a3ffa20f55e94934b21e71c55e69cf84f1
Roberto Konanz <robkonanz@gmail.com>
2019-02-04 12:34:50 -0500

    Core functionality done.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-let questions = ["Who won last Eastern Conference Finals?", "Led by who?", "Who has how many rings?", "Which is currently worn by:", 
-"Who was drafted by:", "Who’s Point Guard is:", "Who also drafted:", "Who went to school with:", "Who was traded to the Pacers from:", 
+let questions = ["Who won the last Eastern Conference Finals?", "Led by who?", "Who has how many rings?", "Which is currently worn by which PG:", 
+"Who was drafted by:", "Who\'s Point Guard is:", "Who also drafted:", "Who went to school with:", "Who was traded to the Pacers from:", 
 "Where he was teammates with former MVP:", "Who was teammates in UCLA with:", "Who was traded from:", "Where he played how many years:", 
-"Which is the same number of rings *answer* has:", "Who had the youngest MVP ever:", "But not unanimous like:", "Who’s splash brother is:", 
-"Who went to Washington State like:", "Who grew up in Australia like:"];
+"Which is the same number of rings *answer* has:", "Who had the youngest MVP ever:", "But not unanimous like:", "Who\'s splash brother is:", 
+"Who went to Washington State like:", "Who grew up in Australia like:", "Who won ROY one year after:", "Who is teammates with the Greek Freak:",
+"Who is an NBA All-Star starter like Sixer\'s:", "Who is from Cameroon like:", "Who is teammates with former DPOY and Finals MVP:", 
+"Who got traded for:", "To the Raptors from the:", "To join 6ft Guard:", "Who wears the jersey #:", "Just like future Hall of Famer:",
+"Who's got how many rings?", "Just like former MVP:", "Who has a Max Contract like:", "Who wants to join the:",
+"Which would be the second big trade of 2019, after the:", "Traded:", "To the mavericks to join European prodigy:", 
+"Who is currently one of how many rookies averaging 20ppg", "And is number *answer* in Rebounds by rookies:", "One spot after:",
+"Who is teammates with Wildcats\' super star:", "That dropped 70 in a game. Third most after Wilt Chamberlain and:", "Who wore which # in the 90's:",
+"Who later wore #24 for how many seasons:", "Which is the same number who was drafted overall?", "Drafted by the Heat just like:",
+"Who is in his last season like:", "Who is the second oldest in the league after:", "Who has won a Dunk Contest like:"];
 
 let correctAnswers = ["nbalogo", "cavaliers", "lebronjames", "three", "chrispaul", "hornets", "kembawalker", "codyzeller", 
-"victoroladipo", "thunder", "russellwestbrook", "kevinlove", "timberwolves", "six", "bulls", "derrickrose", "stephencurry",
-"klaythompson", "aronbaynes", "bensimmons"];
+"victoroladipo", "thunder", "russellwestbrook", "kevinlove", "timberwolves", "six", "bulls", "derrickrose", "stephcurry",
+"klaythompson", "aronbaynes", "bensimmons", "malcolmbrogdon", "giannis", "joelembiid", "pascalsiakam", "kawhileonard",
+"demarderozan", "spurs", "kylelowry", "seven", "carmeloanthony", "zero", "jamesharden", "anthonydavis", "lakers", "knicks", 
+"kristapsporzingis", "lukadoncic", "one", "two", "deandreayton", "devinbooker", "kobebryant", "eight", "ten", "justicewinslow",
+"dwyanewade", "dirknowitzki", "vincecarter", "donovanmitchell"];
 
 var currentQuestion = 0;
 var currentAnswer = 0; 
@@ -22,10 +33,19 @@ $(document).ready(function() {
         $('#question').text(questions[currentQuestion]);
         currentQuestion++;
     });
-    $(".col-sm-3").click(function(e){
+    $(".col-xs-3").click(function(e){
         var id = $(this).attr("id");
         if(id==correctAnswers[currentAnswer]){
-            nextQuestion();
+            if(id==correctAnswers[correctAnswers.length-1]){
+                window.location.replace("file:///Users/robertokonanz/Documents/Ironhack/project1/baller-knowledge/winner.html");
+            }
+            else{
+                $(this).addClass("clicked");
+                nextQuestion();
+            }
+        }
+        else{
+            window.location.replace("file:///Users/robertokonanz/Documents/Ironhack/project1/baller-knowledge/loser.html");
         }
         console.log(id);
     });    
